991c6397cf01c6478ce0eaf3a215299b0d480a24
hashem78 <hashem.olayano@gmail.com>
2021-11-03 12:44:44 +0200

    Alert the user when deleting a question and give them and option to undo the action in 5 seconds

--- lib/widgets/question_tile_widget.dart
+++ lib/widgets/question_tile_widget.dart
@@ -19,8 +19,49 @@ class QuestionTileWidget extends HookWidget {
       onTap: () => shown.value = !shown.value,
       subtitle: useValueListenable(shown) ? Text(question.answer.text) : null,
       trailing: IconButton(
-        onPressed: () async =>
-            await context.read(folderProvider).removeQuestion(question.text),
+        onPressed: () async {
+          final shouldRemove = await showDialog<bool>(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text(
+                    'Are you sure you want to remove this question?'),
+                content: const Text(
+                    'Please be aware that this action is irreversiable'),
+                actions: [
+                  TextButton(
+                    onPressed: () async {
+                      Navigator.pop(context, true);
+                    },
+                    child: const Text('Remove'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context, false);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                ],
+              );
+            },
+          );
+          if (shouldRemove != null && shouldRemove) {
+            final controller = ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: const Text('Removed Question'),
+                duration: const Duration(seconds: 5),
+                action: SnackBarAction(
+                  label: 'Undo',
+                  onPressed: () {},
+                ),
+              ),
+            );
+            final closeReason = await controller.closed;
+            if (closeReason != SnackBarClosedReason.action) {
+              await context.read(folderProvider).removeQuestion(question.uuid);
+            }
+          }
+        },
         icon: const Icon(Icons.delete, color: Colors.red),
       ),
     );
